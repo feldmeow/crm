1a3a85159de9864054214da63f29cf513cc53b16
Jauhen Feldman <feldmeow@gmail.com>
2022-04-17 19:17:54 +0300

    add $ symbol on render (price & finish price)

--- js/main.js
+++ js/main.js
@@ -83,8 +83,8 @@ const createRow = (obj) => {
     <td class="table__cell table__cell_left">${obj.category}</td>
     <td class="table__cell">${obj.units}</td>
     <td class="table__cell">${obj.count}</td>
-    <td class="table__cell">${obj.price}</td>
-    <td class="table__cell">${obj.price * obj.count}</td>
+    <td class="table__cell">$${obj.price}</td>
+    <td class="table__cell">$${obj.price * obj.count}</td>
     <td class="table__cell table__cell_btn-wrapper">
       <button class="table__btn table__btn_pic"></button>
       <button class="table__btn table__btn_edit"></button>
